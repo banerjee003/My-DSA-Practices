bd83d4454a0c3da507617ffb89d7d43c358cfff9
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-07-07 16:03:04 +0530

    Time: 0 ms (100%), Space: 14.9 MB (37.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int>m;
-        vector<int>ans;
         for(int i=0; i < nums.size(); i++){
             int rem = target - nums[i];
             if(m.count(rem)){

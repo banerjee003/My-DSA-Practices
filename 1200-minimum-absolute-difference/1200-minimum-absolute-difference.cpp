b32b307b0c302faf0e2f570cb3363632e3ba90e6
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-07-24 18:23:10 +0530

    Time: 13 ms (86.68%), Space: 36.6 MB (95.79%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -6,6 +6,7 @@ public:
 
         vector<vector<int>>ans;
         int mindiff = INT_MAX;
+        
         for(int i = 0; i < n-1; i++){
             int diff = arr[i+1] - arr[i];
             if(diff == mindiff){

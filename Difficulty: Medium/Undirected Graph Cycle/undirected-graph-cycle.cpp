15fc5f1cc124b4ea7ead17ec6e83eaa8ec1e82aa
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-08-25 15:15:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -0,0 +1,49 @@
+class Solution {
+  public:
+    bool dfs(int curr, int par, vector<vector<int>>&adj, vector<int>&vis){
+        vis[curr] = 1;
+        
+        for(int i : adj[curr]){
+            if(!vis[i]){
+                if(dfs(i, curr, adj, vis)){
+                    return true;
+                }
+            }
+            else{
+                if(i != par){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+  
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        vector<vector<int>>adj(V);
+        for(auto i : edges){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        
+        vector<int>vis(V,0);
+        
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                if(dfs(i, -1, adj, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
+
+
+
+

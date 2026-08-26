bf12bf6c5fdabb8a13a998a990f06d4befd5df78
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-08-26 16:38:18 +0530

    Time: 66 ms (18.16%), Space: 87.9 MB (16.6%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+
+        for(int i : nums){
+            m[i]++;
+            if(m[i] > 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};

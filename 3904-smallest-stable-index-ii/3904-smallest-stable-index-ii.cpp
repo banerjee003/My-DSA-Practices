13592970c0e29284974d7201b364473ceaa2d2bf
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-09-05 11:53:43 +0530

    Time: 18 ms (26.96%), Space: 210.9 MB (35.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int>small(n,0);
+        small[n-1] = nums[n-1];
+        for(int i = n-2; i >= 0; i--){
+            small[i] = min(nums[i], small[i+1]);
+        }
+
+        vector<int>large(n,0);
+        large[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            large[i] = max(nums[i], large[i-1]);
+        }
+
+        for(int i = 0; i < n; i++){
+            int instable = large[i] - small[i];
+            if(instable <= k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};

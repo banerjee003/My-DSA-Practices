4973f5a75eab7a993001faa8fe39fb289fa35325
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-09-20 20:32:58 +0530

    Time: 497 ms (21.5%), Space: 0B (100%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+select
+id,
+case
+    when p_id is NULL then 'Root'
+    when id in (select p_id from Tree) then 'Inner'
+    else 'Leaf'
+end as Type
+from Tree

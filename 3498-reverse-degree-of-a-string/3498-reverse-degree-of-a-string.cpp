e0bc00cb9f7634ec61f8ff92c3b7dee0e9a9b4a0
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-09-20 20:40:48 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -3,8 +3,7 @@ public:
     int reverseDegree(string s) {
         int sum = 0;
         for(int i = 0; i < s.size(); i++){
-            int val = 26 - (s[i] - 'a');
-            sum += val*(i+1);
+            sum += (26 - (s[i] - 'a'))*(i+1);
         }
         return sum;
     }

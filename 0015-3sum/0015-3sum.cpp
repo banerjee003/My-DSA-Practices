cffc7b70592d67a71e60eb6e7d26e227465b5504
PALLAB BANERJEE <pallabbanerjee2003@gmail.com>
2026-09-25 12:06:18 +0530

    Time: 43 ms (82.64%), Space: 29.1 MB (45.57%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,28 +2,34 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>ans;
-
         sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i-1] == nums[i]) continue;
 
-        for(int i = 0; i < n-2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int target = 0 - nums[i];
+            int left = i + 1;
+            int right = n - 1;
 
-            int left = i+1;
-            int right = n-1;
             while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
+                int currSum = nums[left] + nums[right];
+                if(currSum == target){
                     ans.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;   
                     left++;
+                    right--;
+
+                    while(left <  right && nums[left] == nums[left-1])
+                        left++;
+                    
+                    while(left < right && nums[right] == nums[right+1])
+                        right--;
+                }
+                if(currSum < target){
+                    left++;
+                }
+                if(currSum > target){
                     right--;
                 }
-                else if(sum < 0)
-                    left++;
-                else
-                    right--;
             }
         }
         return ans;
